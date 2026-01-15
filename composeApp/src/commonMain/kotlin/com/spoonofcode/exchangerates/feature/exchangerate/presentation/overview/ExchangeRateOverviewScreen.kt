2f53bafbec1120634445b758b7adc112af1c4676
Luczak Bartosz <spoonofcode@gmail.com>
2026-01-15 16:16:13 +0100

    #1 Add components to project

--- composeApp/src/commonMain/kotlin/com/spoonofcode/exchangerates/feature/exchangerate/presentation/overview/ExchangeRateOverviewScreen.kt
+++ composeApp/src/commonMain/kotlin/com/spoonofcode/exchangerates/feature/exchangerate/presentation/overview/ExchangeRateOverviewScreen.kt
@@ -2,8 +2,10 @@ package com.spoonofcode.exchangerates.feature.exchangerate.presentation.overview
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CardDefaults
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.spoonofcode.exchangerates.core.ui.base.BaseScreen
 import com.spoonofcode.exchangerates.core.ui.compose.Paddings.spaceBetweenListElements
+import com.spoonofcode.exchangerates.core.ui.compose.Spacers
 import com.spoonofcode.exchangerates.core.ui.compose.Texts
 import com.spoonofcode.exchangerates.core.ui.ext.koinViewModel
 import com.spoonofcode.exchangerates.feature.exchangerate.domain.model.Rate
@@ -73,9 +76,15 @@ internal class ExchnageRateOverviewScreen(
             shape = MaterialTheme.shapes.large,
             elevation = CardDefaults.elevatedCardElevation(defaultElevation = 2.dp),
         ) {
-            Texts.BL(item.code)
-            Texts.BL(item.currency)
-            Texts.BL(item.mid.toString())
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+            ) {
+                Texts.BL(item.code)
+                Spacers.Weight1(this)
+                Texts.BL(item.currency)
+                Spacers.Weight1(this)
+                Texts.BL(item.mid.toString())
+            }
         }
     }
 }
